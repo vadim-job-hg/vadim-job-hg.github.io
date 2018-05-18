ab6c41a19fd11cd0908b63e7e843c725c1aa0b31
Vadim Kuznetsov <vadimjobhg86@gmail.com>
2018-05-18 10:08:46 +0300

    to vue

---
+++ components/work-single.vue
@@ -0,0 +1,53 @@
+<div id="colorlib-main">
+
+	<div class="colorlib-work">
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-md-6 col-md-offset-3 col-md-pull-3">
+					<span class="heading-meta">Portfolio</span>
+					<h2 class="colorlib-heading animate-box" data-animate-effect="fadeInLeft">Work Single</h2>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-8 image-content">
+					<img class="img-responsive" src="images/work-1.jpg" alt="HTML5 Bootstrap Template by colorlib.com">
+					<img class="img-responsive" src="images/work-2.jpg" alt="HTML5 Bootstrap Template by colorlib.com">
+					<img class="img-responsive" src="images/work-3.jpg" alt="HTML5 Bootstrap Template by colorlib.com">
+					<img class="img-responsive" src="images/work-4.jpg" alt="HTML5 Bootstrap Template by colorlib.com">
+				</div>
+				<div class="col-md-4 sticky-parent">
+					<div id="sticky_item">
+						<div class="project-desc">
+							<h2>Work Name</h2>
+							<span><a href="#">Logo</a>, <a href="#">Brading</a></span>
+							<p>A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth. Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem Ipsum decided to leave for the far World of Grammar.</p>
+							<p class="icon">
+								<span><a href="#"><i class="icon-share3"></i></a></span>
+								<span><a href="#"><i class="icon-eye"></i> 100</a></span>
+								<span><a href="#"><i class="icon-heart"></i> 49</a></span>
+							</p>
+							<p><a href="#" class="btn btn-primary">Live Preview</a> or <a href="#" class="btn-download">Download</a></p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div id="get-in-touch" class="colorlib-bg-color">
+		<div class="colorlib-narrow-content">
+			<div class="row">
+				<div class="col-md-6 animate-box" data-animate-effect="fadeInLeft">
+					<h2>Get in Touch!</h2>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
+					<p class="colorlib-lead">Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
+					<p><a href="#" class="btn btn-primary btn-learn">Contact me!</a></p>
+				</div>
+
+			</div>
+		</div>
+	</div>
+</div>
