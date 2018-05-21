06d19fd935ac21051f3e0863be778a8888ca657c
Vadim Kuznetsov <vadimjobhg86@gmail.com>
2018-05-21 13:57:15 +0300

    to vue

--- js/components.js
+++ js/components.js
@@ -179,23 +179,6 @@ Vue.component('app-work', {
 				</div>
 			</div>
 		</div>
-	</div>
-
-	<div id="get-in-touch" class="colorlib-bg-color">
-		<div class="colorlib-narrow-content">
-			<div class="row">
-				<div class="col-md-6 animate-box" data-animate-effect="fadeInLeft">
-					<h2>Get in Touch!</h2>
-				</div>
-			</div>
-			<div class="row">
-				<div class="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
-					<p class="colorlib-lead">Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
-					<p><a href="#" class="btn btn-primary btn-learn">Contact me!</a></p>
-				</div>
-
-			</div>
-		</div>
 	</div>`
 });
 
@@ -525,18 +508,20 @@ Vue.component('app-contact-page', {
                 <div class="col-md-7 col-md-pull-5">
                     <div class="row">
                         <div class="col-md-10 col-md-offset-1 col-md-pull-1 animate-box" data-animate-effect="fadeInLeft">
-                            <form action="">
+                            <form action="https://formspree.io/vadimjobhg86@gmail.com" method="POST">
                                 <div class="form-group">
-                                    <input type="text" class="form-control" placeholder="Name">
+                                    <input type="text" name = "" class="form-control" placeholder="Name">
                                 </div>
                                 <div class="form-group">
-                                    <input type="text" class="form-control" placeholder="Email">
+                                    <input type="email" name="_replyto" class="form-control" placeholder="Email">
                                 </div>
                                 <div class="form-group">
-                                    <textarea name="" id="message" cols="30" rows="7" class="form-control" placeholder="Message"></textarea>
+                                    <textarea name="message" id="message" cols="30" rows="7" class="form-control" placeholder="Message"></textarea>
                                 </div>
-                                <div class="form-group">
+                                <div class="form-group">                                
                                     <input type="submit" class="btn btn-primary btn-send-message" value="Send Message">
+                                    <input type="hidden" name="_subject" value="contact_me" />
+                                    <input type="hidden" name="_next" value="http://vadim-job-hg.github.io/#/thanks" />
                                 </div>
                             </form>
                         </div>
