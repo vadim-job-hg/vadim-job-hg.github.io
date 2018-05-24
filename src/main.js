32953265e0c7fca5e3a7a565e2594309fd4d3b62
Vadim Kuznetsov <vadimjobhg86@gmail.com>
2018-05-24 16:24:55 +0300

    to vue

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
-const HomeComponent = require('../components/home.vue');
+const AboutComponent = require('../components/about.vue');
 const VueAbout = { template: '<div id="colorlib-main"><app-about></app-about></div>'};
 const VueServices = { template: '<div id="colorlib-main"><app-services></app-services></div>'};
 const VueBlog = { template: '<div id="colorlib-main"><app-blog></app-blog></div>'};
 const VueWork = { template: '<div id="colorlib-main"><app-work></app-work></div>'};
 const VueContact = { template: '<div id="colorlib-main"><app-contact-page></app-contact-page></div>'};
 const routes = [
-    { path: '/', component: HomeComponent },
+    { path: '/', component: AboutComponent },
     { path: '/about', component: VueAbout },
     { path: '/services', component: VueServices },
     { path: '/blog', component: VueBlog },
