a3998913f956bcd2a272886280ff024e9d367ff9
Vadim Kuznetsov <vadimjobhg86@gmail.com>
2018-05-24 17:40:37 +0300

    to vue

--- components/contact.vue
+++ components/contact.vue
@@ -1,2 +1,68 @@
 <template>
+<div id="colorlib-main">
+    <div class="colorlib-contact">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <span class="heading-meta">Read</span>
+                    <h2 class="colorlib-heading animate-box" data-animate-effect="fadeInLeft">Get in Touch</h2>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-5 col-md-push-6">
+                    <div class="colorlib-feature colorlib-feature-sm animate-box" data-animate-effect="fadeInLeft">
+                        <div class="colorlib-icon">
+                            <i class="icon-mail6"></i>
+                        </div>
+                        <div class="colorlib-text">
+                            <p><a href="mailto::vadimjobhg86@gmail.com">vadimjobhg86@gmail.com</a></p>
+                        </div>
+                    </div>
+
+                    <div class="colorlib-feature colorlib-feature-sm animate-box" data-animate-effect="fadeInLeft">
+                        <div class="colorlib-icon">
+                            <i class="icon-map"></i>
+                        </div>
+                        <div class="colorlib-text">
+                            <p>Ukraine Kharkiv</p>
+                        </div>
+                    </div>
+
+                    <div class="colorlib-feature colorlib-feature-sm animate-box" data-animate-effect="fadeInLeft">
+                        <div class="colorlib-icon">
+                            <i class="icon-social-skype"></i>
+                        </div>
+                        <div class="colorlib-text">
+                            <p><a href="skype:vadimus_hg?chat">vadimus_hg</a></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-7 col-md-pull-5">
+                    <div class="row">
+                        <div class="col-md-10 col-md-offset-1 col-md-pull-1 animate-box" data-animate-effect="fadeInLeft">
+                            <form action="https://formspree.io/vadimjobhg86@gmail.com" method="POST">
+                                <div class="form-group">
+                                    <input type="text" name = "" class="form-control" placeholder="Name">
+                                </div>
+                                <div class="form-group">
+                                    <input type="email" name="_replyto" class="form-control" placeholder="Email">
+                                </div>
+                                <div class="form-group">
+                                    <textarea name="message" id="message" cols="30" rows="7" class="form-control" placeholder="Message"></textarea>
+                                </div>
+                                <div class="form-group">
+                                    <input type="submit" class="btn btn-primary btn-send-message" value="Send Message">
+                                    <input type="hidden" name="_subject" value="contact_me" />
+                                    <input type="hidden" name="_next" value="http://vadim-job-hg.github.io/#/thanks" />
+                                </div>
+                            </form>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id="map"></div>
+</div>
 </template>
--- components/services.vue
+++ components/services.vue
@@ -1,2 +1,76 @@
 <template>
+<div class="colorlib-services">
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-md-6 col-md-offset-3 col-md-pull-3">
+					<span class="heading-meta">What I do?</span>
+					<h2 class="colorlib-heading animate-box" data-animate-effect="fadeInLeft">Here are some of my expertise</h2>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-6">
+					<div class="colorlib-feature animate-box" data-animate-effect="fadeInLeft">
+						<div class="colorlib-icon">
+							<i class="icon-tag"></i>
+						</div>
+						<div class="colorlib-text">
+							<h3>Branding</h3>
+							<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. </p>
+						</div>
+					</div>
+
+					<div class="colorlib-feature animate-box" data-animate-effect="fadeInLeft">
+						<div class="colorlib-icon">
+							<i class="icon-pen2"></i>
+						</div>
+						<div class="colorlib-text">
+							<h3>Web Design</h3>
+							<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. </p>
+						</div>
+					</div>
+
+					<div class="colorlib-feature animate-box" data-animate-effect="fadeInLeft">
+						<div class="colorlib-icon">
+							<i class="icon-search3"></i>
+						</div>
+						<div class="colorlib-text">
+							<h3>Search engine optimization</h3>
+							<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. </p>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-6">
+					<div class="colorlib-feature animate-box" data-animate-effect="fadeInLeft">
+						<div class="colorlib-icon">
+							<i class="icon-paperplane"></i>
+						</div>
+						<div class="colorlib-text">
+							<h3>Web Development</h3>
+							<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. </p>
+						</div>
+					</div>
+
+					<div class="colorlib-feature animate-box" data-animate-effect="fadeInLeft">
+						<div class="colorlib-icon">
+							<i class="icon-device-desktop"></i>
+						</div>
+						<div class="colorlib-text">
+							<h3>User Interface</h3>
+							<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. </p>
+						</div>
+					</div>
+
+					<div class="colorlib-feature animate-box" data-animate-effect="fadeInLeft">
+						<div class="colorlib-icon">
+							<i class="icon-help"></i>
+						</div>
+						<div class="colorlib-text">
+							<h3>Help &amp; Support</h3>
+							<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. </p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
