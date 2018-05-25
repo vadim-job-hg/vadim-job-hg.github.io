2b19adeb800b2bca2057b843552c6f8ead963e4f
Vadim Kuznetsov <vadimjobhg86@gmail.com>
2018-05-25 10:51:04 +0300

    to vue

--- components/thanks.vue
+++ components/thanks.vue
@@ -2,6 +2,3 @@
     <h1>Thanks. I will answer you as soon as possible.</h1>
 </template>
 
-<script>
-    this.$route.router.go('/');
-</script>
--- js/build.js
+++ js/build.js
@@ -8362,12 +8362,6 @@ if (module.hot) {(function () {  var hotAPI = require("vue-hot-reload-api")
   }
 })()}
 },{"vue":3,"vue-hot-reload-api":10}],8:[function(require,module,exports){
-;(function(){
-'use strict';
-
-undefined.$route.router.go('/');
-})()
-if (module.exports.__esModule) module.exports = module.exports.default
 var __vue__options__ = (typeof module.exports === "function"? module.exports.options: module.exports)
 if (__vue__options__.functional) {console.error("[vueify] functional components are not supported and should be defined in plain js files using render functions.")}
 __vue__options__.render = function render () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('h1',[_vm._v("Thanks. I will answer you as soon as possible.")])}
