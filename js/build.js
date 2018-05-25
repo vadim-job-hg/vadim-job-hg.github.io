0031324269700db3cabc6f0aa6a7fd1f86e63ca6
Vadim Kuznetsov <vadimjobhg86@gmail.com>
2018-05-25 17:52:49 +0300

    to vue

--- js/build.js
+++ js/build.js
@@ -8442,10 +8442,6 @@ if (module.hot) {(function () {  var hotAPI = require("vue-hot-reload-api")
 },{"vue":3,"vue-hot-reload-api":13}],11:[function(require,module,exports){
 ;(function(){
 "use strict";
-
-setTimeout(function () {
-  window.location.href = "/#/";
-}, 10000);
 })()
 if (module.exports.__esModule) module.exports = module.exports.default
 var __vue__options__ = (typeof module.exports === "function"? module.exports.options: module.exports)
