874581a6856b851f09bf206acd3115653bb13af4
Vadim Kuznetsov <vadimjobhg86@gmail.com>
2018-05-25 12:31:58 +0300

    to vue

--- components/contact-link.vue
+++ components/contact-link.vue
@@ -1,3 +1,17 @@
 <template>
-<div id="get-in-touch" class="colorlib-bg-color"><div class="colorlib-narrow-content"><div class="row"><div data-animate-effect="fadeInLeft" class="col-md-6 animate-box fadeInLeft animated"><h2>Get in Touch!</h2></div></div> <div class="row"><div data-animate-effect="fadeInLeft" class="col-md-6 col-md-offset-3 col-md-pull-3 animate-box fadeInLeft animated"><p class="colorlib-lead">Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p> <p><router-link to="/contact">Contact me!</router-link></p></div></div></div></div>
+<div id="get-in-touch" class="colorlib-bg-color">
+<div class="colorlib-narrow-content">
+<div class="row">
+<div data-animate-effect="fadeInLeft" class="col-md-6 animate-box fadeInLeft animated">
+<h2>Get in Touch!</h2>
+</div>
+</div>
+ <div class="row">
+ <div data-animate-effect="fadeInLeft" class="col-md-6 col-md-offset-3 col-md-pull-3 animate-box fadeInLeft animated">
+ <p class="colorlib-lead">Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
+ <p><router-link to="/contact" class="router-link-exact-active router-link-active">Contact me!</router-link></p>
+ </div>
+ </div>
+ </div>
+ </div>
 </template>
